995e2ff33b1e4b2f02be63a35dec1a5f7d44cc2e
Luke-Finley <luke.finley422@gmail.com>
2025-03-26 06:13:54 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,82 @@
+
+const projects = [
+    {name: "SPLAT", body: "Thumbnails/Splat.png", url: "project1.html", ID: "project1ID"},
+    {name: "Atom's Beta Test", body: "Thumbnails/Atom.png", url: "project2.html", ID: "project2ID"},
+    {name: "Home Invasion", body: "Thumbnails/HomeInvasion.png", url: "project3.html", ID: "project3ID"},
+    {name: "Running Late", body: "Thumbnails/RunningLate.png", url: "project4.html", ID: "project4ID"},
+    {name: "Steamore", body: "Thumbnails/Steamore.png", url: "project5.html", ID: "project5ID"},
+    {name: "Final Showdown 4", body: "Thumbnails/FinalShowdown.png", url: "project6.html", ID: "project6ID"},
+
+];
+
+let i = 0;
+let len = projects.length;
+
+const parentDiv = document.getElementById("primaryParent");
+
+for (; i < len; ) {
+
+    let projectContainer = document.createElement("div");
+    let projectTitle = document.createElement("div");
+    let projectBody = document.createElement("img");
+
+    projectContainer.classList.add('project-container');
+    projectTitle.classList.add('project-title');
+    projectBody.classList.add('project-body');
+
+    projectContainer.appendChild(projectTitle);
+    projectContainer.appendChild(projectBody);
+
+    if(parentDiv != null){
+        parentDiv.appendChild(projectContainer);
+    }
+
+    projectTitle.innerHTML = projects[i].name;
+    projectBody.src = projects[i].body;
+
+    projectContainer.id = projects[i].ID;
+
+    if(parentDiv != null){
+        document.getElementById(projects[i].ID).addEventListener("click", goToProject);
+    }
+
+    i++;
+}
+
+function goToProject() {
+
+    let target = document.getElementById(event.target.parentElement.id);
+
+    let targetID = target.id;
+    
+    let targetString = String(targetID);
+
+    let openWindow = targetString.replace("ID", ".html");
+
+    window.location.href = openWindow;
+}
+
+document.getElementById("portfolio").addEventListener("click", goToPortfolio);
+function goToPortfolio() {
+    window.location.href = "portfolio.html";
+}
+
+document.getElementById("resume").addEventListener("click", goToResume);
+function goToResume() {
+    window.location.href = "resume.html";
+}
+
+document.getElementById("name").addEventListener("click", goToName);
+function goToName() {
+    window.location.href = "index.html";
+}
+
+document.getElementById("portfolioNav").addEventListener("click", goToPortfolio);
+
+document.getElementById("resumeNav").addEventListener("click", goToResume);
+
+document.getElementById("aboutMeNav").addEventListener("click", goToAboutMe);
+function goToAboutMe() {
+    window.scrollTo(0, 800);
+}
+
